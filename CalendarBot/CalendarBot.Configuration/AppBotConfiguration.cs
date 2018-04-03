9225957667f7cb62403f455c930e698763711701
Hiran <hiran.tassinari@gmail.com>
2018-04-03 16:51:28 -0300

    add config project and update some packages

---
+++ CalendarBot/CalendarBot.Configuration/AppBotConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace CalendarBot.Configuration
+{
+    public class AppBotConfiguration
+    {
+        public static string TelegramBotToken;
+        static AppBotConfiguration()
+        {
+            using (var reader = new StreamReader("BotToken.json"))
+            {
+                var json = reader.ReadToEnd();
+                var config = JsonConvert.DeserializeObject<TelegramConfigToken>(json);
+                TelegramBotToken = config.BotToken;
+            }
+        }
+    }
+
+    public class TelegramConfigToken
+    {
+        public string BotToken { get; set; }
+    }
+
+}
